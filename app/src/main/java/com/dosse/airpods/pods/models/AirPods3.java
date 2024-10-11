74adc49df000a63ebbbbc57f84929be356527dd1
allxin <56709386+allxin@users.noreply.github.com>
2024-10-12 03:18:45 +0800

    Update AirPods3.java

--- app/src/main/java/com/dosse/airpods/pods/models/AirPods3.java
+++ app/src/main/java/com/dosse/airpods/pods/models/AirPods3.java
@@ -2,13 +2,13 @@ package com.dosse.airpods.pods.models;
 
 import com.dosse.airpods.pods.Pod;
 
-public class AirPods3 extends RegularPods {
-    public AirPods3(Pod leftPod, Pod rightPod, Pod casePod) {
+public class AirPods4 extends RegularPods {
+    public AirPods4(Pod leftPod, Pod rightPod, Pod casePod) {
         super(leftPod, rightPod, casePod);
     }
 
     @Override
     public String getModel() {
-        return Constants.MODEL_AIRPODS_GEN3;
+        return Constants.MODEL_AIRPODS_GEN4;
     }
 }
